067532bc417314699d777304c2453353d70b327c
surongxiang <517530615@qq.com>
2015-10-08 21:29:45 +0800

    修改导航的超链接

--- js/study.js
+++ js/study.js
@@ -9,22 +9,39 @@ $(".img-universal").click(function () {
 var asd = $(".main").width();
 $(".main").css("marginLeft", -asd / 2);
 
+//var url = "http://news.hfut.club/news/report/";
 
-$.get("js/report.json", function (data, status) {
-    addNews("con-today", 1);
-    addNews("con-tomo", 2);
-    addNews("con-hou", 3);
-    addNews("con-san", 4);
+var url = "./js/report.json";
 
-    function addNews(className, n) {
-        $("." + className).find("li").each(function () {
-            var i = $(this).index();
-            if (data[n][i].title != "null") {
-                $(this).find("a").html(data[n][i].title);
-                $(this).find("a").attr("href", data[n][i].fromurl);
-            } else {
-                $(this).html("暂无安排");
-            }
-        });
+$.ajax({
+    type: "get",
+    url: url,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (data) {
+        addNews("con-today", 1);
+        addNews("con-tomo", 2);
+        addNews("con-hou", 3);
+        addNews("con-san", 4);
+        function addNews(className, n) {
+            $("." + className).find("li").each(function () {
+                var i = $(this).index();
+                if (data[n] != null) {
+                    if (i < data[n].length) {
+                        console.log(i);
+                        $(this).find("a").html(data[n][i].title);
+                        $(this).find("a").attr("href", data[n][i].fromurl);
+                    }
+                    else {
+                        $(this).html("暂无安排");
+                    }
+                } else {
+                    $(this).html("暂无安排");
+                }
+            });
+        }
+    },
+    error: function (err) {
+        console.log(err);
     }
 });

e093b17b58d2eade1f87826889071763ab9a8609
surongxiang <517530615@qq.com>
2015-10-13 23:30:19 +0800

    modify info/service/study page

--- js/info.js
+++ js/info.js
@@ -1,4 +1,99 @@
 /**
  * Created by surongxiang on 15/10/9.
  */
-$(".info-list").find("li:last").css("borderBottom","1px solid #777");
+$(".info-list").find("li:last").css("borderBottom", "1px solid #777");
+$(".info-div").css("width", $(".info-img").width() / 2 - 24);
+$(".info-div").find("img").css("width", $(".info-img").width() / 2 - 24);
+$(".info-div").find("p").css({
+    "width": $(".info-img").width() / 4 - 24
+});
+
+
+//var url1 = "http://news.hfut.club/news/1/1000";
+var url1 = "./js/report.json";
+//var url2 = "./js/report2.json";
+var url2 = "http://news.hfut.club/news/news/51";
+var clientWid = $(window).width();
+var rate = clientWid / 1920;
+
+getNews(url1);
+
+
+function getNews(url) {
+    $.ajax({
+        type: "get",
+        //url: url+i+"/"+item,
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            var item = 16 * rate;
+            Math.round(item);
+            initPagination(item);
+            $(".info-con").css("height", (parseInt($(".info-con").find("li").height()) + 18) * item);
+
+            function handlePaginationClick(page_index, jq) {
+                page_index++;
+                $(".info-list").find("ul").empty();
+                for (var i = (page_index - 1) * item; i <= page_index * item - 1 && i < data.news.length; i++) {
+                    var tab = "<li title='" + data.news[i].aid + "'><span>" + data.news[i].time + "</span><a  data-toggle='modal' data-target='#myModal'>" + data.news[i].title + "</a></li>";
+                    $(".info-list").find("ul").append(tab);
+                }
+
+                return false;
+            }
+
+            /**
+             * Initialisation function for pagination
+             */
+            function initPagination(ii) {
+                // count entries inside the hidden content
+                var num_entries = data.news.length;
+                // Create content inside pagination element
+                $(".searchresult_pagination").pagination(num_entries, {
+                    items_per_page: ii,
+                    num_edge_entries: 1,
+                    callback: handlePaginationClick
+                });
+            }
+
+            //function getCon(id){
+            //    var i;
+            //    for( i =0;i<data.news.length;i++){
+            //        if(data.news[i].aid==id){
+            //            break;
+            //        }
+            //    }
+            //    return data.news[i].title;
+            //}
+
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
+
+$(".info-list").find("ul").find("li").click(function () {
+    getContent(url2, $(this).attr("title"));
+});
+
+
+function getContent(url, aid) {
+    $.ajax({
+        type: "get",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            $(".con-title").html(data.title);
+            $(".con-time").html(data.time);
+            $(".con-con").html(data.content);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
--- js/service.js
+++ js/service.js
@@ -6,6 +6,10 @@ $(document).ready(function () {
     $(".right-main").css("marginLeft", -rightmar / 2);
     var xxx = $(".paint-light").width();
     $(".paint-light").css("marginLeft", -xxx / 2);
+    var clientWid = $(window).width();
+    var rate = clientWid / 1920;
+
+
     var icon1 = $(".icon-1").width();
     var icon2 = $(".icon-2").width();
     var icon3 = $(".icon-3").width();
@@ -15,44 +19,112 @@ $(document).ready(function () {
     var icon7 = $(".icon-7").width();
     var icon8 = $(".icon-8").width();
     var ss = rightmar - icon1 - icon2 - icon3 - icon4 - icon5 - icon6 - icon7 - icon8 - 86;
-    $(".sprite-main").find("li").each(function () {
+    $(".sprite-main").find(".sprite-ul1 li").each(function () {
         if ($(this).index() != 7) {
-            $(this).css("marginRight", ss / 7);
+            //$(this).css("marginRight", ss / 7);
         }
         $(this).mouseover(function () {
             var i = $(this).find("p").eq(1).attr("class");
             i = i.charAt(i.length - 1);
             $(this).find(".book-" + i).addClass("book-shelf-hover");
-            var x = $(".light-" + (i ));
-            x.fadeIn();
+            var x = $(".light-" + (i));
+            x.fadeIn().mouseout(function () {
+                $(this).fadeOut();
+                $(".book-" + i).removeClass("book-shelf-hover");
+            });
             $(x).parents("li").siblings("li").find(".light").fadeOut();
             $(this).siblings("li").find("p").removeClass("book-shelf-hover");
         });
 
-        //$(this).find(".light").mouseout(function () {
-        //    $(this).fadeOut();
-        //    $(this).siblings("p").removeClass("book-shelf-hover");
-        //
-        //});
     });
-    $(".img-all").find(".img-universal").mouseover(function () {
-        var i = $(this).parents(".img-all").attr("class");
-        i = i.charAt(i.length - 1);
-        $(".paint-light-" + i).fadeIn();
-        $(".paint-light-" + i).siblings().fadeOut();
-        $(this).mouseout(function () {
-            $(".paint-light-" + i).fadeOut();
+    $(".sprite-ul1").find("li").css("margin", "0 " + 35 * rate + "px");
+    var iconw = icon1 + icon2 + icon3 + icon4 + icon5 + icon6 + icon7 + icon8 + parseInt($(".sprite-ul1").find("li").css("marginRight")) * 17;
+    $(".sprite-ul1").css("marginLeft", -iconw / 2);
+    $(".sprite-ul2").css("marginLeft", -iconw / 2);
+
+    $(".img-hover").hover(function () {
+        var i = $(this).parents(".img-all").index()-2;
+        $(".paint-light").find("div").eq(i).fadeIn();
+        $(this).mouseleave(function () {
+            $(".paint-light").find("div").eq(i).fadeOut();
         });
     });
 
-
-    $(".img-universal").click(function () {
-        $(this).siblings('a').fadeIn(1, function () {//第一个参数为动画持续时间
+    $(".img-hover").mouseenter(function () {
+        $(this).find('a').fadeIn(1, function () {//第一个参数为动画持续时间
             $(this).find(".img-content").addClass('animated flipInY');//给当前元素添加摇晃效果类
-        });
+        }).css("display","block");
     });
-    $(".img-universal").siblings("a").find(".img-content").mouseout(function () {
-        $(this).parents("a").hide();
+    $(".img-hover").mouseleave(function () {
+    //    $(this).find("img").mouseleave(function () {
+    //        $(this).siblings("a").find(".img-content").removeClass('animated flipInY');
+    //        $(this).siblings("a").hide();
+    //    });
+        $(this).find(".img-content").removeClass('animated flipInY');
+        $(this).find("a").hide();
     });
 
+
+    //$(".img-universal").hover(function () {
+    //    var i = $(this).siblings("a").find(".img-content").attr("class");
+    //    i = i[20];
+    //    $(".paint-light-" + i).fadeIn();
+    //    $(".paint-light-" + i).siblings().fadeOut();
+    //    $(this).mouseout(function () {
+    //        $(".paint-light-" + i).fadeOut();
+    //    });
+    //});
+
+    $(".tag-1").css("top", 25 * rate);
+    $(".tag-3").css("top", 25 * rate);
+    $(".img-2").find("img").css("marginTop", 49 * rate);
+    $(".img-4").find("img").css("marginTop", 49 * rate);
+    $(".tag-all").css("width", 105 * rate);
+
+    $(".img-con-1").css({
+        width: $(".img-universal").eq(0).width()+5,
+        height: $(".img-universal").eq(0).width()+3,
+        "marginRight": parseInt($(".img-universal").eq(0).css("paddingLeft"))+15,
+        "marginTop": -parseInt($(".img-universal").eq(0).css("paddingLeft"))-parseInt($(".img-universal").eq(0).width())-17
+    });
+
+    setTimeout(function () {
+        $(".img-con-2").css({
+            width: $(".img-universal").eq(1).width(),
+            height: $(".img-universal").eq(1).width()/1.19,
+            marginRight: parseInt($(".img-universal").eq(1).css("paddingLeft"))+15,
+            marginTop: -parseInt($(".img-universal").eq(1).css("paddingLeft"))-parseInt($(".img-universal").eq(1).width()/1.19)-15
+        });
+        $(".img-con-4").css({
+            width: $(".img-universal").eq(3).width(),
+            height: $(".img-universal").eq(3).width(),
+            marginRight: parseInt($(".img-universal").eq(3).css("paddingLeft"))+15,
+            marginTop: -parseInt($(".img-universal").eq(3).css("paddingLeft"))-parseInt($(".img-universal").eq(3).width())-15
+        });
+    },100);
+
+    $(".img-con-3").css({
+        width: $(".img-universal").eq(2).width()+2,
+        height: $(".img-universal").eq(2).width()/0.55,
+        marginRight: parseInt($(".img-universal").eq(2).css("paddingLeft"))+15,
+        marginTop: -parseInt($(".img-universal").eq(2).css("paddingLeft"))-parseInt($(".img-universal").eq(2).width()/0.56)-18
+    });
+
+
+
+
+    $(".book-shelf").find("img").css("width", $(".book-shelf").width() + 33);
+
+    $(".tag-1").click(function () {
+        setTimeout(window.open('http://book.hfutonline.net/'), 100);
+    });
+    $(".tag-2").click(function () {
+        setTimeout(window.open('http://sunnyradio.hfutonline.net/radio/index/'), 100);
+    });
+    $(".tag-3").click(function () {
+        setTimeout(window.open('http://upan.hfutonline.net/upload'), 100);
+    });
+    $(".tag-4").click(function () {
+        setTimeout(window.open('http://sketch.hfutonline.net/sketch/index'), 100);
+    });
 });

ecf2ee433a92968c8f35fec3c25b4895e21bfc37
surongxiang <517530615@qq.com>
2015-10-15 00:01:35 +0800

    fix some bugs and add some new pics

--- js/info.js
+++ js/info.js
@@ -2,24 +2,21 @@
  * Created by surongxiang on 15/10/9.
  */
 
+
+
 $(document).ready(function () {
 
 
-    //var url1 = "http://news.hfut.club/news/1/1000";
-    var url1 = "./js/report.json";
-//var url2 = "./js/report2.json";
-    var url2 = "http://news.hfut.club/news/news/51";
+    var url1 = "http://news.hfut.club/news/news/1/5000";
+    //var url1 = "./js/report.json";
+    //var url2 = "./js/report2.json";
+    var url2 = "http://news.hfut.club/news/news/";
     var clientWid = $(window).width();
     var rate = clientWid / 1920;
-
     getNews(url1);
 
-    $(".info-list").find("ul").find("li").click(function () {
-        getContent(url2, $(this).attr("title"));
-    });
 
 
-    $(".info-list").find("li:last").css("borderBottom", "1px solid #777");
     $(".info-div").css("width", $(".info-img").width() / 2 - 40);
     $(".info-div").find("img").css("width", $(".info-img").width() / 2 - 40);
     $(".info-div").find("p").css({
@@ -38,13 +35,16 @@ $(document).ready(function () {
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function (data) {
-                var item = 14 * rate;
+                var item = 11 * rate;
                 item = Math.round(item);
                 initPagination(item);
                 var a = $(".info-con").find("li").height();
                 var b = $(".info-con").find("li").css("paddingTop");
                 var c = parseInt(a) + parseInt(b) * 2;
-                $(".info-con").css("height", c * item);
+                var itemnum = parseInt(item)+1;
+                var xh = parseInt(c*itemnum)-31;
+                $(".info-con").css("height", xh);
+                //$(".info-list").find("li:last").css("borderBottom", "1px solid #ddd");
                 function handlePaginationClick(page_index, jq) {
                     page_index++;
                     $(".info-list").find("ul").empty();
@@ -69,16 +69,28 @@ $(document).ready(function () {
                     });
                 }
 
-                //function getCon(id){
-                //    var i;
-                //    for( i =0;i<data.news.length;i++){
-                //        if(data.news[i].aid==id){
-                //            break;
-                //        }
-                //    }
-                //    return data.news[i].title;
-                //}
+                $(".info-list").find("ul").find("li").click(function () {
+                    getContent(url2, $(this).attr("title"));
+                });
 
+                function getContent(url, aid) {
+                    $.ajax({
+                        type: "get",
+                        url: url+aid,
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (data) {
+                            console.log(url+aid);
+                            $(".con-title").html(data.title);
+                            $(".con-time").html(data.time);
+                            $(".con-con").html(data.content);
+                            $("#artibody").removeClass("content");
+                        },
+                        error: function (err) {
+                            console.log(err);
+                        }
+                    });
+                }
 
             },
             error: function (err) {
@@ -88,22 +100,6 @@ $(document).ready(function () {
     }
 
 
-    function getContent(url, aid) {
-        $.ajax({
-            type: "get",
-            url: url,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                $(".con-title").html(data.title);
-                $(".con-time").html(data.time);
-                $(".con-con").html(data.content);
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        });
-    }
+
 
 });

408030238ffdc31ee4b0d820299f216f017e984b
Shafley <shafleyxia@gmail.com>
2015-10-15 09:50:38 +0800

    add animation for index page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-	
+	$('.nav').hide();
+	$('.content').hide();
+
+	var fontImg = new Image();
+	fontImg.src = "images/index/font.gif";
+	fontImg.onload = function (){
+		setTimeout(function(){
+			$('.nav').show();
+			$('.content').show();
+
+			$(".m-modal-pic").addClass('m-clock-pic');
+			setTimeout(function(){
+				$(".m-modal-pic").removeClass('m-clock-pic');
+				$(".m-modal-pic").addClass('m-block');
+				$('.m-modal-font').hide();
+				$('.m-modal-box').addClass('m-modal-transform');
+			}, 5000);
+		}, 3000);
+	}
 });

b47015f9e6d50ffd4ae40ee787a57dae84d7e385
zhujun24 <zhujun87654321@gmail.com>
2015-10-15 09:11:49 +0800

    zhujun24 first commit

--- js/special.js
+++ js/special.js
@@ -1,33 +1,46 @@
-(function(){
-	//启动函数
-	init();
-	function init(){
-		getSiteMore();
-		// addScrollBar();
-	}
-	function getSiteMore(){
-		$('.get-site-more').click(function(){
-			var current = $(this);
-			var site_more = $(this).parent().find('.site-more');
-			site_more.slideToggle('slow', function(){
-				current.fadeOut();
-			});
-		});
-		$('.fix-silk img').click(function(){
-			var site_more = $(this).parent().parent().find('.site-more');
-			var get_more = $(this).parent().parent().find('.get-site-more');
-			if(site_more.css('display') == 'block'){
-				get_more.fadeIn();
-			}
-			site_more.slideToggle('slow', function(){
-				if(site_more.css('display') == 'block'){
-					get_more.fadeOut();
-				}
-			});
-		});
-	}
-	function addScrollBar(){
-		$('.main').mCustomScrollbar();
-	}
+(function () {
+    //启动函数
+    init();
+    function init() {
+        getSiteMore();
+        // addScrollBar();
+    }
+
+    function getSiteMore() {
+        $('.get-site-more').click(function () {
+            var current = $(this);
+            var site_more = $(this).parent().find('.site-more');
+            site_more.slideToggle('slow', function () {
+                current.fadeOut();
+            });
+        });
+        $('.fix-silk img').click(function () {
+            var site_more = $(this).parent().parent().find('.site-more');
+            var get_more = $(this).parent().parent().find('.get-site-more');
+            if (site_more.css('display') == 'block') {
+                get_more.fadeIn();
+            }
+            site_more.slideToggle('slow', function () {
+                if (site_more.css('display') == 'block') {
+                    get_more.fadeOut();
+                }
+            });
+        });
+    }
+
+    function addScrollBar() {
+        $('.main').mCustomScrollbar();
+    }
+
+    $('.site-description').each(function (index, item) {
+        var obj = $(item);
+        obj.hover(function () {
+            obj.addClass('flipInY animated');
+        }, function () {
+        });
+        obj.click(function () {
+            obj.removeClass('flipInY animated');
+        })
+    });
 
 })()
